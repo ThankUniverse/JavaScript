e51aca3dae3b8af0a3d3f9be0cc65534c193ebae
Michael Tolis <tolism.info@gmail.com>
2018-01-11 18:10:24 +0100

    update Javascript BOM

---
+++ 10-DOM/Files/files/app.js
@@ -0,0 +1,13 @@
+//Exercice 1
+//============
+
+document.body.classList.remove("bg-aqua"); // enlever le background CSS
+document.body.classList.add("bg-olive"); // Rajouter le background CSS
+var firstP = document.getElementById("first-paragraph").classList.remove("bg-lime"); //enlever la couleur du fond dans l'ID : First-paragraph
+var firstP = document.getElementById("first-paragraph").classList.remove("gray");  //enlever la couleur grise du texte dans l'ID : First-paragraph
+var firstP = document.getElementbyId("first-paragraph").classList.add("aqua"); //Ajouter la couleur aqua à l'ID : First-paragraph
+
+
+
+
+
